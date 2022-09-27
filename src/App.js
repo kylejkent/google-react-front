6e8885674b6a826d0d22f1597f877bd0d7ec3ae2
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-27 16:07:17 +0700

    Updated class to className

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 function App() {
   return (
 		<div class="wrap-sm">
-      <img class="logofront" src="img/google.png" alt="Google logo" />
+      <img className="logofront" src="img/google.png" alt="Google logo" />
       <form action="/results" method="post">
         <input type="search" name="search" autofocus />
-        <button class="primary">Search</button>
+        <button className="primary">Search</button>
       </form>
     </div>
   )
