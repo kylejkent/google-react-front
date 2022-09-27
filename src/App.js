cce89dbaa23124f688bbbe2b89d6cce411219a4c
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-27 16:29:54 +0700

    Updated function App and turned it into a Search component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-function App() {
-  return (
+import React from 'react'
+
+class Search extends React.Component {
+	state = {
+		input: ''
+	}
+
+	render() {
+		return (
 		<div class="wrap-sm">
-      <img className="logofront" src="img/google.png" alt="Google logo" />
-      <form action="/results" method="post">
-        <input type="search" name="search" autofocus />
-        <button className="primary">Search</button>
-      </form>
-    </div>
-  )
+		  <img className="logofront" src="img/google.png" alt="Google logo" />
+		  <form action="/results" method="post">
+		    <input type="search" name="search" autofocus />
+		    <button className="primary">Search</button>
+		  </form>
+		</div>
+		)
+	}
+
 }
 
-export default App;
+export default Search
