8618a6de6a466c919f1d3ed0f94b7efaba1394a3
kylejkent <kylejkent@gmail.com>
2022-09-27 16:29:13 +0700

    add Results component

---
+++ src/results.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+class Results extends React.Component {
+	state = {
+		search: 'Java'
+	}
+	render() {
+    return (
+			<>
+								// <!-- *** NAV PARTIAL START *** -->
+					{> navresults}
+					// <!-- *** NAV PARTIAL END *** -->
+					<section className="resultsPage">
+						<div className="searchPath">
+							<span className="searchPath">{qtyText}</span>
+						</div>
+					</section>
+					// <!-- *** RESULTS LINK ARRAY START *** -->
+					<section class="searchResults">
+						{{#each results as |result|}}
+						<div>
+							<small>{{result.url}}</small>
+							<h1>
+								<a href="{{result.url}}" target="_blank"
+									>{{result.title}}</a
+								>
+							</h1>
+							<p>{{result.description}}</p>
+							<ul>
+								{{#each links as |link|}}
+								<li><a href="{{link.url}}" target="_blank">{{link.title}}</a></li>
+								{{/each}}
+							</ul>
+						</div>
+						{{/each}}
+					</section>
+					// <!-- *** RESULTS LINK ARRAY END *** -->
+			</>
+    )
+  }
+}
+
+export default Results;
