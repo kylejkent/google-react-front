159992a9ca058b09b63fe008cc49d225b07fe2c1
kylejkent <kylejkent@gmail.com>
2022-09-28 18:20:00 +0700

    bring css for result

--- src/Result.js
+++ src/Result.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 class Result extends React.Component {
 	render() {
+		const styles = {
+			result: {
+				marginBottom: '40px'
+			},
+			unorderedList: {
+				paddingLeft: '1px',
+				margin: '0px',
+				listStyleType: 'none'
+			},
+			list: {
+				display: 'inline-block',
+				marginRight: '6px'
+			}
+		}
     return (
 			<>
-				<section className="searchResults">
+				<section className="searchResults" style={styles.result}>
 						<small>{this.props.result.url}</small>
 						<h1>
 						<a href={this.props.result.url} target="_blank"
@@ -12,14 +26,13 @@ class Result extends React.Component {
 						>
 						</h1>
 						<p>{this.props.result.description}</p>
-							<ul>
+							<ul style={styles.unorderedList}>
 							{this.props.result.links.map((link, i) => (
-								<li key={i}>
-									<a href={link.url} target="_blank">{link.title}</a>
+								<li key={i} >
+									<a href={link.url} target="_blank" style={styles.list}>{link.title} </a>
 								</li>
 							))}
 							</ul>
-					})
 				</section>
 			</>
     )
