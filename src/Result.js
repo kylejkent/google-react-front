7f1c668c1ecfa07745d11b16ea962cdd7ec5b457
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-28 18:40:50 +0700

    Added the links for each result and their needed loop

--- src/Result.js
+++ src/Result.js
@@ -5,11 +5,19 @@ class Result extends React.Component {
 
 	render() {
 		return (
-
 			<section className="singleResult">
-				<h1> {this.props.result.title} </h1>
-				<span> {this.props.result.url} </span>
+				<span className="sm"> {this.props.result.url} </span>
+				<a className="noline" href={this.props.result.url}><h2> {this.props.result.title} </h2></a>
 				<p> {this.props.result.description}</p>
+				<ul>
+					{this.props.result.links.map((link, index) => {
+						return (
+							<li>
+								<a class="sm" href={link.url}> {link.title} </a>
+							</li>
+						)
+					})}
+				</ul>
 			</section>
 		)}
 }
