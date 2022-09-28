84f10848e50cc6342486ca57174f7e5e88af5b60
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-28 17:16:38 +0700

    Updated State with complete results, updated render, added loop, linked to Result

--- src/results.js
+++ src/results.js
@@ -1,60 +1,51 @@
 import React from 'react'
+import Result from './Result'
 
 class Results extends React.Component {
 	state = {
-		search: 'Java',
-		qtyResults: 3
+		search: '',
+		qtyResults: 0,
+		results: [ {
+			url: 'mysite.com',
+			title: 'My Site',
+			description: 'This is the description of my site.',
+			links: {
+				}
+			},
+			{
+				url: 'mysite22.com',
+				title: 'My 2nd Site',
+				description: 'This is thfadsadfe description of my site.',
+				links: {
+					}
+				},
+		]
 	}
+
 	storeSearch = (e) => {
 		console.log(e.target.value)
 		this.setState({
 			search: e.target.value
 		})
 	}
+
 	render() {
     return (
 			<>
-{/*** NAV PARTIAL START *** will add as a component/}
-					<nav>
-					<a href="/" className="googleLogo resultsLogo"><img
-					className="googleLogo resultsLogo"
-					src="/img/google.png"
-					alt="Google logo"
-					/></a>
-
-					<form action="/results" method="post">
-					<input type="search" name="search" onKeyUp={e => this.storeSearch(e)}  />
-					<button className="primary resultsPage">Search</button>
-					</form>
-					</nav>
-{/*** NAV PARTIAL END ***/}
-					<section className="resultsPage">
+				<section className="resultsPage">
 						<div className="searchPath">
-							<span className="searchPath">{this.state.qtyResults}</span>
+							<span className="searchPath">{this.state.qtyResults} search results found</span>
 							<span className="searchPath">{this.state.search}</span>
 						</div>
 					</section>
-{/*** RESULTS LINK ARRAY START ***/}
-{/** comment out results/}
-					{/<section class="searchResults">/}
-						{/{{#each results as |result|}}/}
-						<div>
-							<small>{{result.url}}</small>
-							<h1>
-								<a href="{{result.url}}" target="_blank"
-									>{{result.title}}</a
-								>
-							</h1>
-							<p>{{result.description}}</p>
-							<ul>
-								{{#each links as |link|}}
-								<li><a href="{{link.url}}" target="_blank">{{link.title}}</a></li>
-								{{/each}}
-							</ul>
-						</div>
-						{{/each}}
-					</section>
-{/*** RESULTS LINK ARRAY END ***/}
+					{this.state.results.map((result, index) => {
+						return (
+							<Result result={result} key={index}/>
+						)
+					})}
+
+
+
 			</>
     )
   }
