f9086fea244ca754f86b45cd4e90d0a169f0184a
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-29 17:39:44 +0700

    Updated Search html elements with conditional CSS classes

--- src/Search.js
+++ src/Search.js
@@ -4,12 +4,13 @@ import App from './App.js'
 class Search extends React.Component {
 	render() {
 		return (
-		<div className="wrap-sm">
-		  <img className="logofront" src="img/google.png" alt="Google logo" />
+		<div className={this.props.searched == false ? "wrap-sm" : "header"}>
 
-		    <input type="search" name="search" onKeyUp={e => this.props.storeSearch(e)}  />
-		    <button className="primary" onClick={() => this.props.submitSearch()}>Search</button>
-
+		  <img className={ this.props.searched == false ? "logofront" : "logoresults"} src="img/google.png" alt="Google logo" />
+			<div>
+		    <input type="search" name={this.props.searched == false ? "search" : "searchres2"} value={this.props.search} onChange={e => this.props.storeSearch(e)}   />
+		    <button className={this.props.searched == false ? "" : "resultspage"} onClick={() => this.props.submitSearch()}>Search</button>
+			</div>
 		</div>
 		)
 	}
