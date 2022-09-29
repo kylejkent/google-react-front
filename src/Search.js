55c7094b917f7f7b3644a72e744e597f0b3b9396
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-29 17:41:12 +0700

    Added link to image

--- src/Search.js
+++ src/Search.js
@@ -6,7 +6,7 @@ class Search extends React.Component {
 		return (
 		<div className={this.props.searched == false ? "wrap-sm" : "header"}>
 
-		  <img className={ this.props.searched == false ? "logofront" : "logoresults"} src="img/google.png" alt="Google logo" />
+		  <a href="/"><img className={ this.props.searched == false ? "logofront" : "logoresults"} src="img/google.png" alt="Google logo" /> </a>
 			<div>
 		    <input type="search" name={this.props.searched == false ? "search" : "searchres2"} value={this.props.search} onChange={e => this.props.storeSearch(e)}   />
 		    <button className={this.props.searched == false ? "" : "resultspage"} onClick={() => this.props.submitSearch()}>Search</button>
