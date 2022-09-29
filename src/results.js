65de992f6fcb881102a2b6f4cd23826ae8b40a0b
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-09-29 15:36:29 +0700

    Removed state and methods, imported App, updated props

--- src/results.js
+++ src/results.js
@@ -1,48 +1,8 @@
 import React from 'react'
 import Result from './Result'
+import App from './App'
 
 class Results extends React.Component {
-	state = {
-		search: '',
-		qtyResults: 0,
-		results: [ {
-			url: 'https://www.w3schools.com/js/default.asp',
-			title: 'JavaScript Tutorial - W3School',
-			description: 'Well organized and easy to understand Web building tutorials with lots	of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java',
-			links: [
-				{title: "JavaScript Introduction", url: "https://www.w3schools.com/js/js_intro.asp"},
-				{title: "JS Functions", url: "https://www.w3schools.com/js/js_functions.asp"},
-				{title: "JavaScript Examples", url: "https://www.w3schools.com/js/js_examples.asp"}
-				]
-			},
-			{
-				url: 'mysite22.com',
-				title: 'My 2nd Site',
-				description: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting langauge for Web pages, CouchDB, and Adobe Acrobat.',
-				links: [
-					{title: "Alright Site", url: "www.alrightsite.com"},
-					{title: "Super Cool Site", url: "www.supercoolsite.com"}
-				]
-			},
-			{
-				url: 'https://www.w3schools.com/js/default.asp',
-				title: 'JavaScript Tutorial - W3School',
-				description: 'Well organized and easy to understand Web building tutorials with lots	of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java',
-				links: [
-					{title: "JavaScript Introduction", url: "https://www.w3schools.com/js/js_intro.asp"},
-					{title: "JS Functions", url: "https://www.w3schools.com/js/js_functions.asp"},
-					{title: "JavaScript Examples", url: "https://www.w3schools.com/js/js_examples.asp"}
-					]
-				}
-		]
-	}
-
-	storeSearch = (e) => {
-		console.log(e.target.value)
-		this.setState({
-			search: e.target.value
-		})
-	}
 
 	render() {
     return (
@@ -62,11 +22,11 @@ class Results extends React.Component {
 	    	</header>
 				<section>
 						<div className="numres">
-							<span className="numres">{this.state.qtyResults} search results found</span>
-							<span className="numres">{this.state.search}</span>
+							<span className="numres">{this.props.values.qtyResults} search results found</span>
+							<span className="numres">{this.props.values.search}</span>
 						</div>
 					</section>
-					{this.state.results.map((result, index) => {
+					{this.props.results.map((result, index) => {
 						return (
 							<Result result={result} key={index}/>
 						)
